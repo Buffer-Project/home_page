fc741462a5e8ff47a24c075f175ee75e274131d7
German <germanaltairac@hotmail.com>
2020-10-10 16:06:45 -0300

    fix duplicated commits

---
+++ css/style.css
@@ -0,0 +1,13 @@
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200&family=PT+Serif&display=swap');
+body {
+    font-family: 'Nunito', sans-serif;;
+    color: white;
+    font-size: 23px;
+    background: black;
+    text-align: center;
+}
+
+.homeMainInfoSegment {
+    margin-left: 10%;
+    margin-right: 10%;
+}
